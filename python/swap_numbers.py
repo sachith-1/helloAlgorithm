268b7db7e16ce45c7ec4c54b2fbfd2ccabd42494
kishmat <kishmat17@gmail.com>
2021-10-14 11:18:29 +0545

    added swap_numbers.py

---
+++ python/swap_numbers.py
@@ -0,0 +1,9 @@
+"""
+    A program to swap two numbers without the use of extra temp variable
+"""
+a = int(input("Enter the positive value of a : "))
+b = int(input("Enter the positive value of b : "))
+a = a + b
+b = a - b
+a = a - b
+print(f"The value of a is : {a} \nThe value of b is : {b}")
