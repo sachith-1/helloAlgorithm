65bde14aaddbd8c39713d7e871a81d28f86aa8ba
DEV270201 <devansh.js@somaiya.edu>
2021-10-20 21:31:10 +0530

    added permutationOfStrings

--- python/mergesort.py
+++ python/mergesort.py
@@ -1,42 +1,33 @@
-def merge_sort(collection: list) -> list:
-    """Pure implementation of the merge sort algorithm in Python
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
-    Examples:
-    >>> merge_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-    >>> merge_sort([])
-    []
-    >>> merge_sort([-2, -5, -45])
-    [-45, -5, -2]
-    """
+def merge_sort(unsorted_list):
+    if len(unsorted_list) <= 1:
+        return unsorted_list
 
-    def merge(left: list, right: list) -> list:
-        """merge left and right
-        :param left: left collection
-        :param right: right collection
-        :return: merge result
-        """
+    middle = len(unsorted_list) // 2
+    left_list = unsorted_list[:middle]
+    right_list = unsorted_list[middle:]
 
-        def _merge():
-            while left and right:
-                yield (left if left[0] <= right[0] else right).pop(0)
-            yield from left
-            yield from right
-
-        return list(_merge())
-
-    if len(collection) <= 1:
-        return collection
-    mid = len(collection) // 2
-    return merge(merge_sort(collection[:mid]), merge_sort(collection[mid:]))
+    left_list = merge_sort(left_list)
+    right_list = merge_sort(right_list)
+    return list(merge(left_list, right_list))
 
 
-if __name__ == "__main__":
-    import doctest
 
-    doctest.testmod()
-    user_input = input("Enter numbers separated by a comma:\n").strip()
-    unsorted = [int(item) for item in user_input.split(",")]
-    print(*merge_sort(unsorted), sep=",")
+def merge(left_half,right_half):
+
+    res = []
+    while len(left_half) != 0 and len(right_half) != 0:
+        if left_half[0] < right_half[0]:
+            res.append(left_half[0])
+            left_half.remove(left_half[0])
+        else:
+            res.append(right_half[0])
+            right_half.remove(right_half[0])
+    if len(left_half) == 0:
+        res = res + right_half
+    else:
+        res = res + left_half
+    return res
+
+unsorted_list = [64, 34, 25, 12, 22, 11, 90]
+
+print(merge_sort(unsorted_list))
